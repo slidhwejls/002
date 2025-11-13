e17673b28d0db9e2e08076eaf96229e5d2882c03
slidhwejls <thaoquachavihm@gmail.com>
2025-11-12 20:13:39 -0500

    Update link.js

--- ojbk/link.js
+++ ojbk/link.js
@@ -1,1 +1,25 @@
 
+ 
+var link = {
+    kyH5   : 'https://ky5066.com',
+    mkH5  : 'https:833mk.vip'
+}
+
+function clickFun(key) {
+    let u = navigator.userAgent;
+    let Agents = ["Android", "iPhone", "webOS", "BlackBerry", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+    let flag = true;
+    for (let i = 0; i < Agents.length; i++) {
+        if (u.indexOf(Agents[i]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+
+    if (key === 'ky'){
+      window.open(link['kyH5'])
+    }else if(key === 'mk'){ 
+        console.log(link['mkH5'])
+      window.open(link['mkH5'])
+    }
+}
